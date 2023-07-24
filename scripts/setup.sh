8d70864677a33380b8d46a3cb9a9e565f194a0c4
Sashanoraa <sasha@noraa.gay>
2023-07-24 04:20:01 -0400

    Setup inits dbs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,4 @@
 
 podman-compose exec synapse register_new_matrix_user -c /cfg/homeserver.yaml -u test -p test -a  
 podman-compose exec synapse register_new_matrix_user -c /cfg/homeserver.yaml -u pk -p kitty --no-admin
-psql -h localhost -U synapse -c 'CREATE DATABASE plural_kitty OWNER synapse;'
+sqlx database setup
